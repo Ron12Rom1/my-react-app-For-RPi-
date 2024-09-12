8d0f5efda173b1ae64ebd0b6f7018d88ce7135f7
ronronbruk <ronronbruk@gmail.com>
2024-09-12 08:15:17 +0000

    add "phazaz"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
 
   return (
-    <div>
+    <div id='counter'>
       <h1>Counter: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <div className='counter-btn'>
+        <button class="btns" onClick={() => setCount(count + 1)}>Increment</button>
+        <button class="btns" onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+      <button class="btns" id="resetBTN" onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 
