0152cf7ffae24b1fe434181c7070a8bd44171cb6
ronronbruk <ronronbruk@gmail.com>
2024-09-14 17:42:50 +0000

    add hom page

---
+++ src/HomePage.jsx
@@ -0,0 +1,21 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1711767637.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2086955466.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1861926121.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1491542164.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2001313410.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:812681279.
+
+import React, { useState, useEffect } from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <>
+    <ul>
+        <li><a href="/counter">Counter Page</a></li>
+    </ul>
+    </>
+  );
+}
+
+export default HomePage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:3358136934.
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from './assets/pages/App.jsx';
+import HomePage from './HomePage.jsx';
 import './main.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={"Hello"} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/counter" element={<App />} />
     </Routes>
     </BrowserRouter> 
